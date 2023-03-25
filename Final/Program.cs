43d28746dd650e7e34ccb44e1e3d2c6298d6149a
Danila <danik.hoo@mail.ru>
2023-03-25 17:11:21 +0300

    Add final homework

---
+++ Final/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] array = { "hello", "2 ", "world", ":-)" };
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
